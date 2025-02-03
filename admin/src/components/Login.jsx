e8d5ff707c2d1f5d05b3972760ad0b875a2a3f7b
Faiza <faizamohammed72730882@gmail.com>
2025-02-02 13:12:49 -1200

    committed

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -1,10 +1,35 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-empty */
+import axios from "axios";
+
+import { useState } from "react";
+import { backendUrl } from "../App";
+import { toast } from "react-toastify";
+
 /* eslint-disable no-unused-vars */
-const Login = () => {
+const Login = ({setToken}) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const onSubmitHandler = async (e) => {
     try {
       e.preventDefault();
-    } catch (error) {}
+
+      const response = await axios.post(backendUrl + '/api/user/admin', {
+        email,
+        password,
+      });
+      if (response.data.success) {
+        setToken(response.data.token)
+        toast.error(response.data.message)
+
+      } else{
+        toast.error(response.data.message)
+      }
+    } catch (error) {
+        console.log(error)
+        toast.error(error.message)
+    }
   };
   return (
     <div className="min-h-screen flex items-center justify-center w-full">
@@ -16,6 +41,8 @@ const Login = () => {
               Email Adress
             </p>
             <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
               className="rounded-md  w-full px-3 py-2 border border-gray-300 outline-none"
               type="email"
               placeholder="your@gmial.com"
@@ -24,16 +51,18 @@ const Login = () => {
           <div>
             <p>Password</p>
             <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
               className="rounded-md  w-full px-3 py-2 border border-gray-300 outline-none"
-              type="email"
+              type="password"
               placeholder="Enter your password"
             />
           </div>
           <button
             className="mt-2 w-full py-2 px-4 rounded-md text-white bg-black"
-            type="submit">
-            {" "}
-            Login{" "}
+            type="submit"
+          >
+            Login
           </button>
         </form>
       </div>
